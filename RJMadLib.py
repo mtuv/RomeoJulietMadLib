cf3183219da7d1348db312e3edb336d41802b897
mtuv <63384676+mtuv@users.noreply.github.com>
2020-04-13 22:52:22 -0400

    Mad Lib Of Romeo And Juliet

---
+++ RJMadLib.py
@@ -0,0 +1,31 @@
+
+def madlib():
+    noun_plr1 = str(raw_input("Enter a NOUN (PLURAL)\n"))
+    place1 = str(raw_input("Enter a PLACE\n"))
+    noun1 = str(raw_input("Enter a NOUN\n"))
+    noun_plr2 = str(raw_input("Enter a NOUN (PLURAL)\n"))
+    noun2 = str(raw_input("Enter a NOUN\n"))
+    adj1 = str(raw_input("Enter an ADJECTIVE\n"))
+    verb1 = str(raw_input("Enter a VERB\n"))
+    num1 = str(raw_input("Enter a NUMBER\n"))
+    adj2 = str(raw_input("Enter an ADJECTIVE\n"))
+    bdy_prt = str(raw_input("Enter a BODY PART\n"))
+    verb2 = str(raw_input("Enter a VERB\n"))
+
+    print("Two " + noun_plr1 + ", both alike in dignity,\n" +
+          "In fair " + place1 + ", where we lay our scene,\n"+
+          "From ancient " + noun1 + " break to new mutiny,\n" +
+          "Where civil blood makes civil hands unclean.\n" +
+          "From forth the fatal loins of these two foes\n" +
+          "A pair of star-cross`d " + noun_plr2 + " take their life;\n" +
+          "Whole misadventured piteous overthrows\n" +
+          "Do with their " + noun2 + " bury their parents` strife.\n" +
+          "The fearful passage of their " + adj1 + " love,\n" +
+          "And the continuance of their parents` rage,\n" +
+          "Which, but their children`s end, nought could " + verb1 + ",\n" +
+          "Is now the " + num1 + " hours` traffic of our stage;\n" +
+          "The which if you with " + adj2 + bdy_prt + " attend,\n" +
+          "What here shall " + verb2 + ", our toil shall strive to mend.")
+
+
+madlib()
